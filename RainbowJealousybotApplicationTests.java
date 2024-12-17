9045b8d2e288e6d854f24355f8d6379c2f5f59ee
Oivand <jasuwiwk@gmail.com>
2024-12-17 21:05:57 +0300

    Add files via upload

---
+++ RainbowJealousybotApplicationTests.java
@@ -0,0 +1,13 @@
+package oivand.rainbow_jealousybot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RainbowJealousybotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
